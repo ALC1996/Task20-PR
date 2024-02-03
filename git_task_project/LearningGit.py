b6c8ad73a71198f6293de083a1dbeb515d7abdcd
Alex Crosby <alexcrosbywork@hotmail.com>
2024-02-03 15:31:43 +0000

    #renamed file to match code and added comments

---
+++ git_task_project/LearningGit.py
@@ -0,0 +1,10 @@
+# Displays message to user
+print("Welcome, Git is awesome!")
+# Asks for user if they use git
+user_input = input("Do you currently use Git? yes/no ").lower()
+# If user answers yes, display message
+if user_input == "yes":
+    print("Great! I'm currently trying to learn Git!")
+# If user answers no, display message
+else:
+    print("No problem, I'm currently learning Git, lets learn together!")
